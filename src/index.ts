d8a39ca1d3808e14b8cca6cf0cb969e7584c86ef
Artyom Nazaroff <artyom.nazaroff@yandex.ru>
2025-09-25 08:51:50 +0700

    Добавил первые запросы

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,87 @@ import express from 'express';
 const app = express();
 const port = 3003;
 
-// Middleware для парсинга JSON
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+const HTTP_STATUSES = {
+    OK_200: 200,
+    CREATED_201: 201,
+    NO_CONTENT_204: 204,
+    BAD_REQUEST_400: 400,
+    NOT_FOUND_404: 404,
+    INTERNAL_SERVER_ERROR_500: 500
+} as const;
 
-// Простой маршрут для проверки
-app.get('/', (req, res) => {
-	const a = 4;
-	if (a > 5) {
-		res.send('Привет, мир!');
-	} else res.send('Привет, мир!!!!');
+const db = {
+	courses: [
+		{ id: 1, title: 'front-end' },
+		{ id: 2, title: 'back-end' },
+		{ id: 3, title: 'design' },
+		{ id: 4, title: 'devops' },
+	],
+};
+
+// Middleware для парсинга JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/courses', (req, res) => {
+	let foundCourses = db.courses;
+	if (req.query.title) {
+		foundCourses = foundCourses.filter(
+			(el) => el.title.indexOf(req.query.title as string) > -1
+		);
+	}
+	res.json(foundCourses);
+});
+app.get('/courses/:id', (req, res) => {
+	const foundCourse = db.courses.find((el) => el.id === +req.params.id);
+
+	if (!foundCourse) {
+		res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+		return;
+	}
+
+	res.json(foundCourse);
+});
+app.post('/courses', (req, res) => {
+	if (!req.body.title.trim()) {
+		res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+		return;
+	}
+
+	const createdCourse = {
+		id: +new Date(),
+		title: req.body.title,
+	};
+	db.courses.push(createdCourse);
+	res.status(HTTP_STATUSES.CREATED_201).json(createdCourse);
+});
+app.delete('/courses/:id', (req, res) => {
+	db.courses = db.courses.filter((el) => el.id !== +req.params.id);
+
+	res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
+app.put('/courses/:id', (req, res) => {
+	if (!req.body.title.trim()) {
+		res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+		return;
+	}
+
+	const foundCourse = db.courses.find((el) => el.id === +req.params.id);
+
+	if (!foundCourse) {
+		res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+		return;
+	}
+
+	foundCourse.title = req.body.title;
+
+	res.json(foundCourse);
 });
 
 // Обработка ошибок
 app.use((err: any, req: any, res: any, next: any) => {
 	console.error(err.stack);
-	res.status(500).send('Что-то пошло не так!');
+	res.status(HTTP_STATUSES.INTERNAL_SERVER_ERROR_500).send('Что-то пошло не так!');
 });
 
 app.listen(port, () => {
